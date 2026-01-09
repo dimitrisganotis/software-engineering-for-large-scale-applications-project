207097918dcee409ddb0a8cb35bad96be5129e3b
Dimitris Ganotis <dimitris@thebrinkagency.com>
2026-01-09 19:11:20 +0100

    Initial commit

---
+++ backend/src/main/java/com/project/dass/DassApplication.java
@@ -0,0 +1,13 @@
+package com.project.dass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DassApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/dass/DassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
